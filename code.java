a73cc3e15541ead5b87bb2f1b68bb3dcd7bce926
steukk <lerastoik@gmail.com>
2025-09-17 13:50:13 +0300

    code.java

---
+++ code.java
@@ -0,0 +1,14 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        // создаём список, используемый как стек
+        Stack<Character> stack = new Stack<>(); //создание стек символов
+
+        m.push('a');  // добавляем 'a' в стек
+        m.push('b');  // добавляем 'b' в стек
+        m.push('c');  // добавляем 'c' в стек
+
+        System.out.println(m); // выводим стек
+    }
+}
