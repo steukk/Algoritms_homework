298b4d0e482a3f52e445a5aa2d5efb59e933ca0f
steukk <lerastoik@gmail.com>
2025-09-17 14:32:23 +0300

    code.cc

--- code.cc
+++ code.cc
@@ -1,16 +1,32 @@
 #include <iostream>
 #include <vector>
-
+#include <any>
+#include <string>
+#include <tuple>
+#include <set>
+#include <map>
 
 int main() {
-    std::vector<char> m; // создаём пустой вектор, который будем использовать как стек
+    std::vector<std::any> m; //список, содержащие разные типы данных
+    m.push_back(1); // int-целое число
+    m.push_back(3.21); // double-число с плавающей точкой
+    m.push_back(std::string("Привет!")); // string- строка
+    m.push_back(true); // bool-логический тип
+    m.push_back(std::vector<int>{7, 8, 9}); // vector<int>-список, изменяемая последовательность
+    m.push_back(std::make_tuple(7, 8, 9)); // tuple(int, int, int)-кортеж, неизменяемая последовательность
+    m.push_back(std::set<int>{7, 8, 9}); // set<int>-множество элементов, неупорядоченное
 
-    m.push_back('a');  // добавляем 'a' в стек
-    m.push_back('b');  // добавляем 'b' в стек
-    m.push_back('c');  // добавляем 'c' в стек
+
+    
+int main() {
+    std::vector<char> stack; // создаём пустой вектор, который будем использовать как стек
+
+    stack.push_back('a');  // добавляем 'a' в стек
+    stack.push_back('b');  // добавляем 'b' в стек
+    stack.push_back('c');  // добавляем 'c' в стек
 
     // выводим элементы стека
-    for (char c : m) {
+    for (char c : stack) {
         std::cout << c << " ";
     }
     std::cout << std::endl;
