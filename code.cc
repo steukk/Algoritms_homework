cebf344d595c0243f090447ec99d89c54b33aa60
steukk <lerastoik@gmail.com>
2025-09-26 09:30:16 +0300

    code.cc

--- code.cc
+++ code.cc
@@ -1,35 +1,28 @@
+//пример двоичной кучи на с++
 #include <iostream>
 #include <vector>
-#include <any>
-#include <string>
-#include <tuple>
-#include <set>
-#include <map>
+#include <algorithm>
+using namespace std;
 
 int main() {
-    std::vector<std::any> m; //список, содержащие разные типы данных
-    m.push_back(1); // int-целое число
-    m.push_back(3.21); // double-число с плавающей точкой
-    m.push_back(std::string("Привет!")); // string- строка
-    m.push_back(true); // bool-логический тип
-    m.push_back(std::vector<int>{7, 8, 9}); // vector<int>-список, изменяемая последовательность
-    m.push_back(std::make_tuple(7, 8, 9)); // tuple(int, int, int)-кортеж, неизменяемая последовательность
-    m.push_back(std::set<int>{7, 8, 9}); // set<int>-множество элементов, неупорядоченное
+    vector<int> heap = {3, 1, 2};
+    make_heap(heap.begin(), heap.end()); // Создание кучи
+    pop_heap(heap.begin(), heap.end());  // Извлечение максимума
+    heap.pop_back();
+    cout << heap.front() << endl;  // Теперь максимальным элементом будет 2
+    return 0;
+}
 
 
-    
+//Пример hash таблицы
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
 int main() {
-    std::vector<char> stack; // создаём пустой вектор, который будем использовать как стек
-
-    stack.push_back('a');  // добавляем 'a' в стек
-    stack.push_back('b');  // добавляем 'b' в стек
-    stack.push_back('c');  // добавляем 'c' в стек
-
-    // выводим элементы стека
-    for (char c : stack) {
-        std::cout << c << " ";
-    }
-    std::cout << std::endl;
-
+    unordered_map<string, int> map;
+    map["one"] = 1;
+    map["two"] = 2;
+    cout << map["one"] << endl;  // Выведет 1
     return 0;
 }
