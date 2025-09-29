c51e672bf54963b87dae91236657d49ff23fae13
steukk <lerastoik@gmail.com>
2025-09-29 14:50:01 +0300

    Update code.java

--- code.java
+++ code.java
@@ -12,6 +12,23 @@ public class Main {
     }
 }
 
+//Пример биноминальной кучи
+class BinomialNode {
+    int key;
+    List<BinomialNode> children = new ArrayList<>();
+    BinomialNode(int k) { key = k; }
+}
+BinomialNode n = new BinomialNode(5);
+System.out.println(n.key); // 5
+
+// Фибоначчиева куча
+class FibNode {
+    int key;
+    List<FibNode> children = new ArrayList<>();
+    FibNode(int k) { key = k; }
+}
+FibNode node = new FibNode(7);
+System.out.println(node.key); // 7
 
 
 //пример hash таблицы на java
