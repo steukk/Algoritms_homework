453cb15a66058d4d78467eeb8f81b90293142e1d
steukk <lerastoik@gmail.com>
2025-09-29 14:42:08 +0300

    Update code.py

--- code/code.py
+++ code/code.py
@@ -6,6 +6,20 @@ heapq.heappush(heap, 1)
 heapq.heappush(heap, 2)
 print(heapq.heappop(heap))  # Выведет 1 (минимум)
 
+#Биномиальная куча:
+class BinomialNode:
+    def __init__(self, key):
+        self.key = key
+        self.children = []
+node = BinomialNode(5)
+
+#Фибоначчиева куча
+class FibNode:
+    def __init__(self, key):
+        self.key = key
+        self.child = []
+node = FibNode(7)
+
 
 #Пример hash таблицы:
 map = {"one": 1, "two": 2}
